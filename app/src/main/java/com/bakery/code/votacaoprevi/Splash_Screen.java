e86aa5713e1ec1118079827d7f474d9f96b5a806
BrunoTomassettiCouto <Bruno.t.couto@icloud.com>
2017-08-14 15:13:05 -0300

    Splash Screen pronta

--- app/src/main/java/com/bakery/code/votacaoprevi/Splash_Screen.java
+++ app/src/main/java/com/bakery/code/votacaoprevi/Splash_Screen.java
@@ -1,10 +1,11 @@
 package com.bakery.code.votacaoprevi;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash_Screen extends AppCompatActivity {
+public class Splash_Screen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
