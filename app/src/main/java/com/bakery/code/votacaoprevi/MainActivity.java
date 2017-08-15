2058cfa607032353b927b4d03191af5941876022
gamarote <gamarote@hotmail.com>
2017-08-15 18:54:36 -0300

    Landscape disabled

--- app/src/main/java/com/bakery/code/votacaoprevi/MainActivity.java
+++ app/src/main/java/com/bakery/code/votacaoprevi/MainActivity.java
@@ -2,11 +2,13 @@ package com.bakery.code.votacaoprevi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
--- app/src/main/java/com/bakery/code/votacaoprevi/Splash_Screen.java
+++ app/src/main/java/com/bakery/code/votacaoprevi/Splash_Screen.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Splash_Screen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
 
