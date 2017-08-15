65266438ffd2fc33736d89b9aa52e9a6af912397
BrunoTomassettiCouto <Bruno.t.couto@icloud.com>
2017-08-15 14:37:42 -0300

    Criação ultimas telas

---
+++ app/src/main/java/com/bakery/code/votacaoprevi/Fim_voto.java
@@ -0,0 +1,14 @@
+package com.bakery.code.votacaoprevi;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fim_voto extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fim_voto);
+    }
+}
