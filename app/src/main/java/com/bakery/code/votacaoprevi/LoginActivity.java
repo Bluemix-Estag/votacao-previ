fe6240f8fa4b7f164f4278c77c2c8f8f70d78aff
BrunoTomassettiCouto <Bruno.t.couto@icloud.com>
2017-08-16 17:03:30 -0300

    ReCaptcha

--- app/src/main/java/com/bakery/code/votacaoprevi/LoginActivity.java
+++ app/src/main/java/com/bakery/code/votacaoprevi/LoginActivity.java
@@ -1,16 +1,29 @@
 package com.bakery.code.votacaoprevi;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.concurrent.Executor;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +33,9 @@ public class LoginActivity extends AppCompatActivity {
     private TextView senha;
     private TextView erro_mens;
     private ImageView erro_img;
+    private TextCaptcha textCaptcha;
+    private ImageView captcha;
+    private Button Call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +48,13 @@ public class LoginActivity extends AppCompatActivity {
         erro_mens = (TextView) findViewById(R.id.User_invalido);
         erro_img = (ImageView) findViewById(R.id.INVALID_IMG);
         senha = (TextView) findViewById(R.id.SENHA);
+        //captcha = (ImageView) findViewById(R.id.CAPTCHA);
+
+        //textCaptcha = new TextCaptcha(600, 150, 6, TextCaptcha.TextOptions.LETTERS_ONLY);;
+        //captcha.setImageBitmap(textCaptcha.getImage());
+
+
+
 
         continuar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +68,21 @@ public class LoginActivity extends AppCompatActivity {
                     erro_img.setImageResource(android.R.drawable.ic_delete);
                 }else{
                     if (ValidaCPF.isCPF(Login) == true){
-                        Intent intent = new Intent(LoginActivity.this, opcoes_de_voto.class);
-                        startActivity(intent);
+                        SafetyNet.getClient(LoginActivity.this).verifyWithRecaptcha("6LcO8iwUAAAAAPQqSDrqcgoQrg0wUYI2de_gLl_-").addOnSuccessListener(LoginActivity.this, new OnSuccessListener() {
+                                    @Override
+                                    public void onSuccess(Object o) {
+                                        Intent intent = new Intent(LoginActivity.this, opcoes_de_voto.class);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        erro_mens.setText("Erro no ReCaptcha!");
+                                        erro_img.setImageResource(android.R.drawable.ic_delete);
+                                    }
+                                });
+
                     }else if (ValidaCPF.isCPF(Login) == false){
                         Log.i("login","");
                         erro_mens.setText("CPF inválido!");
